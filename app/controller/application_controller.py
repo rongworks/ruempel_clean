d7bda325e6c298598dfa098f857d13fea2119b9c
Mathias Rong <mathiasrong@gmail.com>
2020-02-01 09:13:47 +0100

    use case invalid page given

--- app/controller/application_controller.py
+++ app/controller/application_controller.py
@@ -5,14 +5,21 @@ class ApplicationController:
 
     def __init__(self, request):
         self.params = request.args
+        self.pages_set = ['test']
 
     def home(self):
         return render_template('base.html', title='Home')
 
     def pages(self):
         page_slug = self.params['slug']
-        if page_slug:
-            return render_template('pages/'+page_slug+'.html')
+        if self.__is_valid_page(page_slug) :
+            return render_template('pages/' + page_slug + '.html')
+        else:
+            return self.not_found()
+
 
     def not_found(self):
         return render_template('base.html', title='Home', error='GIBBES NICH!!'), 404
+
+    def __is_valid_page(self, slug):
+        return slug in self.pages_set
